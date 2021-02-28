53e340a895604aa8d20457e2f37ebb93f16db029
Matias Pompilio <matiaspompilio97@gmail.com>
2021-02-28 00:20:38 -0300

    parsing html content from url

---
+++ helpers/htmlParser.js
@@ -0,0 +1,47 @@
+const cheerio = require('cheerio');
+
+const parseSourceUrl = (source = '') => {
+  const regex = /\w*\.js/i;
+  let result = '';
+  const sourceParsed = regex.exec(source);
+  if(sourceParsed) {
+    result = sourceParsed[0];
+  }
+  return result;
+};
+
+/**
+ * Given a html string gets parsed content
+ * @param  {String} html The html data
+ * @return {Object}      Object containing the html content
+ */
+const getHtmlContent = (html) => {
+  /* Load html string in cheerio dom */
+  const $ = cheerio.load(html);
+
+  /* Getting page title from the html loaded */
+  const title =  $("title").text();
+
+  /* Getting script resources */
+  const scripts = [];
+  $("script").each(function() {
+    const src = $(this).attr('src');
+    const sourceParsed = parseSourceUrl(src)
+    if(sourceParsed) {
+      scripts.push(sourceParsed);
+    };
+  });
+
+  const length = Buffer.byteLength(html, 'utf-8');
+
+  const content = {
+    title,
+    scripts,
+    length,
+  };
+  return content;
+};
+
+module.exports = {
+  getHtmlContent,
+};
